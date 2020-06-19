d10f686d71ef5be1c7ab26bb6761c1585815a4ae
Al Newkirk <al@iamalnewkirk.com>
2020-06-19 09:23:07 -0400

    Zing Spawner (#81)

--- lib/Zing/Spawner.pm
+++ lib/Zing/Spawner.pm
@@ -1,4 +1,4 @@
-package Zing::Launcher;
+package Zing::Spawner;
 
 use 5.014;
 
@@ -9,7 +9,6 @@ use registry 'Zing::Types';
 use routines;
 
 use Data::Object::Class;
-use Data::Object::Space;
 
 extends 'Zing::Worker';
 
@@ -20,14 +19,9 @@ extends 'Zing::Worker';
 method handle(Str $name, HashRef $data) {
   return $self if !$data->{scheme};
 
+  $self->spawn($data->{scheme});
+
   my $class = $data->{scheme}[0];
-
-  {
-    my $space = Data::Object::Space->new($class);
-    my $build = $space->build(@{$data->{scheme}[1]});
-
-    $build->execute for 1..($data->{scheme}[2] || 1);
-  }
 
   ($self->{launched}{$class} ||= 0)++;
 
