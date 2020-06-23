d7d1a346d02a4dc30dcb34d2820e583ed9ece45e
Al Newkirk <al@iamalnewkirk.com>
2020-06-23 13:47:41 -0400

    Implement Zing::Zang::Single

---
+++ lib/Zing/Zang/Single.pm
@@ -0,0 +1,19 @@
+package Zing::Zang::Single;
+
+use 5.014;
+
+use strict;
+use warnings;
+
+use registry;
+use routines;
+
+use Data::Object::Class;
+use Data::Object::ClassHas;
+
+extends 'Zing::Single';
+extends 'Zing::Zang';
+
+# VERSION
+
+1;
