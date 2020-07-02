6ea14972562b46c09c452a2445ed45c8948cf9fe
Al Newkirk <al@iamalnewkirk.com>
2020-07-02 00:35:29 -0400

    Prevent fork-bombing in Spawner (#151)

--- lib/Zing/Launcher.pm
+++ lib/Zing/Launcher.pm
@@ -21,20 +21,10 @@ method handle(Str $name, HashRef $data) {
   return $self if !$data->{scheme};
 
   my $class = $data->{scheme}[0];
+  my $space = Data::Object::Space->new($class);
+  my $build = $space->build(@{$data->{scheme}[1]});
 
-  {
-    my $space = Data::Object::Space->new($class);
-    my $build = $space->build(@{$data->{scheme}[1]});
-
-    $build->execute for 1..($data->{scheme}[2] || 1);
-  }
-
-  ($self->{launched}{$class} ||= 0)++;
-
-  # due to the unforeseen (but expected) consequences of loading too many
-  # packages into memory, a launcher should kill itself after loading some
-  # arbitrary number of packages
-  $self->winddown if keys %{$self->{launched}} > 50;
+  $build->execute for 1..($data->{scheme}[2] || 1);
 
   return $self;
 }
--- lib/Zing/Spawner.pm
+++ lib/Zing/Spawner.pm
@@ -19,16 +19,9 @@ extends 'Zing::Worker';
 method handle(Str $name, HashRef $data) {
   return $self if !$data->{scheme};
 
-  $self->spawn($data->{scheme});
+  my $fork = $self->spawn($data->{scheme});
 
-  my $class = $data->{scheme}[0];
-
-  ($self->{launched}{$class} ||= 0)++;
-
-  # due to the unforeseen (but expected) consequences of loading too many
-  # packages into memory, a launcher should kill itself after loading some
-  # arbitrary number of packages
-  $self->winddown if keys %{$self->{launched}} > 50;
+  do {0} while $fork->sanitize;
 
   return $self;
 }
